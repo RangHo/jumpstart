43e6520bc8a600c6e999cd82ea8cd3dfbc03b59b
RangHo Lee <hello@rangho.me>
2024-05-14 19:46:03 +0900

    feat: add rootfs and initramfs handlers

---
+++ api/pxe/rootfs.go
@@ -0,0 +1,21 @@
+package pxerootfshandler
+
+import (
+	"net/http"
+
+	"github.com/RangHo/jumpstart/pkg/artifact"
+)
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	// Get the stream and architecture from the parameters
+	stream := r.URL.Query().Get("stream")
+	architecture := r.URL.Query().Get("arch")
+
+	url, err := artifact.GetRootfs(stream, architecture)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+
+	http.Redirect(w, r, url, http.StatusTemporaryRedirect)
+}
